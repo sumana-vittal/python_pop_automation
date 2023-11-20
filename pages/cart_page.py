fda9b4cd0c88c33d95b6417da88ea0f70235922d
sumana vittal <v.sumana@gmail.com>
2023-11-19 17:08:49 -0800

    Lesson7: wait implementation for search results

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -7,4 +7,4 @@ class CartPage(Page):
     CART_EMPTY_MESSAGE = (By.CSS_SELECTOR, "h1[class*='StyledHeading']")
 
     def verify_cart_empty_message(self, expected_message):
-        cart_empty_message = self.verify_text(expected_message, *self.CART_EMPTY_MESSAGE)
+        self.verify_text(expected_message, *self.CART_EMPTY_MESSAGE)
