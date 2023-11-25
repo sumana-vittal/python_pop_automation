7bee071afc7526cdad79ec18446cbf838a00b090
sumana vittal <v.sumana@gmail.com>
2023-11-24 18:14:14 -0800

    adding changes to the environment file for the browser supoort firefox

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 
 from app.application import Application
@@ -10,9 +11,14 @@ def browser_init(context):
     """
     :param context: Behave context
     """
-    driver_path = ChromeDriverManager().install()
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+
+   # service = Service(executable_path="/Users/raman/Documents/Careerist/QA/python_pop_automation/geckodriver")
+    driver_path = GeckoDriverManager().install()
     service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    context.driver = webdriver.Firefox(service=service)
 
     context.driver.wait = WebDriverWait(context.driver, 10)
     context.driver.maximize_window()
